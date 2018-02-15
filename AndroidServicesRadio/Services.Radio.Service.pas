ed7c0c06e6cfecf1aa3083130273afe459c60fec
El Diablo <shadelinemailbox-war3evo@yahoo.com>
2018-02-15 16:25:21 -0500

    Now has dependencies from this sourcecode: https://github.com/War3Evo/KastriFree

--- AndroidServicesRadio/Services.Radio.Service.pas
+++ AndroidServicesRadio/Services.Radio.Service.pas
@@ -11,6 +11,7 @@ uses
   AndroidApi.JNI.GraphicsContentViewText,
   Androidapi.JNI.Os,
   Services.Radio,
+  DW.Androidapi.JNI.LocalBroadcastManager, // for TJLocalBroadcastManager
   DateUtils;
 
 
@@ -22,9 +23,9 @@ type
       const Intent: JIntent; Flags, StartId: Integer): Integer;
   private
     { Private declarations }
-    NotificationCenter1: TCustomNotificationCenter; // was TNotificationCenter;
+    //NotificationCenter1: TCustomNotificationCenter; // was TNotificationCenter;
     FRadyo: TRTLRadio;
-    procedure LaunchNotification;
+    procedure DoMessage(const AMsg: string);
   public
     { Public declarations }
   end;
@@ -42,53 +43,26 @@ implementation
 uses
   Androidapi.JNI.App,
   Androidapi.JNI.JavaTypes;
-{
+
+const
+  cServiceMessageAction = 'com.embarcadero.services.plaStreamer'; // Your Main Application
+  cServiceMessageParamMessage = 'message';
+
+
 procedure StatusProc(pszData : string;Progress:Integer);
-var
-  MyNotification: TNotification;
 begin
-  MyNotification := TDM.NotificationCenter1.CreateNotification;
-  try
-    MyNotification.Name := 'StatusProc';
-    MyNotification.Title := 'Status Information';
-    MyNotification.AlertBody := Format('%s: %s',['Status:', pszData]);
-    TDM.NotificationCenter1.PresentNotification(MyNotification);
-  finally
-    MyNotification.Free;
-  end;
-end; }
+  DM.DoMessage(Format('%s:%s:%d',['STATUS', pszData, Progress]));
+end;
 
 procedure BroadcastInfoProc(pszBroadcastName,pszBitRate:string);
-var
-  MyNotification: TNotification;
 begin
-  InfoData := Format('FUNC:%s:BROADCAST=%s:BITRATE=%s',['BroadcastInfoProc',pszBroadcastName,pszBitRate]);
-  MyNotification := DM.NotificationCenter1.CreateNotification;
-  try
-    MyNotification.Name := 'BroadcastInfoProc';
-    MyNotification.Title := 'Broadcast Information';
-    MyNotification.AlertBody := Format('FUNC:%s:BROADCAST=%s:BITRATE=%s',['BroadcastInfoProc',pszBroadcastName,pszBitRate]);
-    DM.NotificationCenter1.PresentNotification(MyNotification);
-  finally
-    MyNotification.Free;
-  end;
+  DM.DoMessage(Format('FUNC:%s:BROADCAST=%s:BITRATE=%s',['BroadcastInfoProc',pszBroadcastName,pszBitRate]));
 end;
 
 
 procedure BroadcastMetaProc(pszData : string);
-var
-  MyNotification: TNotification;
 begin
-  MetaData := Format('FUNC:%s:%s',['BroadcastMetaProc',pszData]);
-  MyNotification := DM.NotificationCenter1.CreateNotification;
-  try
-    MyNotification.Name := 'BroadcastMetaProc';
-    MyNotification.Title := 'Broadcast Information';
-    MyNotification.AlertBody := Format('FUNC:%s:%s',['BroadcastMetaProc',pszData]);
-    DM.NotificationCenter1.PresentNotification(MyNotification);
-  finally
-    MyNotification.Free;
-  end;
+  DM.DoMessage(Format('FUNC:%s:%s',['BroadcastMetaProc',pszData]));
 end;
 
 procedure TDM.AndroidServiceCreate(Sender: TObject);
@@ -96,11 +70,11 @@ begin
   MetaData := '';
   InfoData := '';
   FRadyo := TRTLRadio.Create;
-  //FRadyo.SetStatusProc(StatusProc);
+  FRadyo.SetStatusProc(StatusProc);
   FRadyo.SetBroadcastInfoProc(BroadcastInfoProc);
   FRadyo.SetBroadcastMetaProc(BroadcastMetaProc);
   FRadyo.SetOwner(Self);
-  //FRadyo.SetStreamURL('http://s6.nexuscast.com/tunein/cactiradio.pls');
+  //FRadyo.SetStreamURL('http://s6.nexuscast.com/tunein/cactiradio.pls');  //sets at StartIntent
 end;
 
 procedure TDM.AndroidServiceDestroy(Sender: TObject);
@@ -113,8 +87,6 @@ end;
 
 function TDM.AndroidServiceStartCommand(const Sender: TObject;
   const Intent: JIntent; Flags, StartId: Integer): Integer;
-var
-  MyNotification: TNotification;
 begin
   // recieve commands
   if Assigned(Intent) then
@@ -127,41 +99,6 @@ begin
     else if Intent.getAction.equalsIgnoreCase(StringToJString('StartIntent')) = true then
     begin
       FRadyo.SetStreamURL(JStringToString(Intent.getData.toString));
-      Result := TJService.JavaClass.START_STICKY; // rerun service if it stops
-    end
-    else if Intent.getAction.equalsIgnoreCase(StringToJString('GetMetaData')) = true then
-    begin
-      // notification MetaData
-      {if MetaData <> '' then
-      begin
-        MyNotification := NotificationCenter1.CreateNotification;
-        try
-          MyNotification.Name := 'MetaData';
-          MyNotification.Title := 'Broadcast Information';
-          MyNotification.AlertBody := MetaData;
-          NotificationCenter1.PresentNotification(MyNotification);
-        finally
-          MyNotification.Free;
-        end;
-      end; }
-      LaunchNotification;
-      Result := TJService.JavaClass.START_STICKY; // rerun service if it stops
-    end
-    else if Intent.getAction.equalsIgnoreCase(StringToJString('GetInfoData')) = true then
-    begin
-      // notification InfoData
-      if InfoData <> '' then
-      begin
-        MyNotification := NotificationCenter1.CreateNotification;
-        try
-          MyNotification.Name := 'InfoData';
-          MyNotification.Title := 'Broadcast Information';
-          MyNotification.AlertBody := InfoData;
-          NotificationCenter1.PresentNotification(MyNotification);
-        finally
-          MyNotification.Free;
-        end;
-      end;
       Result := TJService.JavaClass.START_STICKY; // rerun service if it stops
     end
     else if Intent.getAction.equalsIgnoreCase(StringToJString('PlayRadio')) = true then
@@ -186,37 +123,20 @@ begin
 end;
 
 {
-procedure TDM.LaunchNotification;
-var
-  MyNotification: TNotification;
+function GetLogTime: string;
 begin
-    if(NotificationCenter1.Supported) then
-    begin
-       MyNotification := NotificationCenter1.CreateNotification;
-       try
-         MyNotification.Name := 'ServiceNotification';
-         MyNotification.Title := 'Android Service Notification';
-         MyNotification.AlertBody := 'My Notification works using GetMetaData LOL!';
-         MyNotification.FireDate := Now;
-         NotificationCenter1.PresentNotification(MyNotification);
-       finally
-         MyNotification.Free;
-       end;
-    end;
+  Result := FormatDateTime('mm-dd hh:nn:ss.zzz', Now);
 end; }
 
-procedure TDM.LaunchNotification;
+procedure TDM.DoMessage(const AMsg: string);
 var
-  Intent: JIntent;
+  LIntent: JIntent;
 begin
-  Intent := TJIntent.Create;
-  //Intent.setClassName(TAndroidHelper.Activity.getBaseContext,
-    //TAndroidHelper.StringToJString('com.embarcadero.services.plaStreamer'));
-  Intent.setType(StringToJString('text/pas'));
-  Intent.setAction(TJIntent.JavaClass.ACTION_VIEW);
-  Intent.putExtra(TJIntent.JavaClass.EXTRA_TEXT, StringToJString('Wow This is an Intent And May work! GetMetaData'));
-  //if TAndroidHelper.Activity.getPackageManager.queryIntentActivities(Intent, TJPackageManager.JavaClass.MATCH_DEFAULT_ONLY).size > 0 Then
-  TAndroidHelper.Activity.startActivity(Intent);
+  // Sends a local broadcast that the app can receive. This *should* be thread-safe since it does not access any outside references
+  LIntent := TJIntent.JavaClass.init(StringToJString(cServiceMessageAction));
+  //LIntent.putExtra(StringToJString(cServiceMessageParamMessage), StringToJString(GetLogTime + ':' + AMsg));
+  LIntent.putExtra(StringToJString(cServiceMessageParamMessage), StringToJString(AMsg));
+  TJLocalBroadcastManager.JavaClass.getInstance(TAndroidHelper.Context).sendBroadcast(LIntent);
 end;
 
 end.
